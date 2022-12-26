5d2f4388372a37a59def712204a05aa677f55444
Jackson Spindle <jacksonmilesspindle@gmail.com>
2022-12-26 16:02:16 -0500

    css styling for the Profile component and edit profile form

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { updateAuth } from "../store";
+import Hoodie from "./Hoodie";
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls } from "@react-three/drei";
 
 const Profile = () => {
   const { auth } = useSelector((state) => state);
@@ -60,115 +63,138 @@ const Profile = () => {
     });
   };
 
-  useEffect(() => {
-    console.log(auth);
-  }, []);
-
   return (
     <div>
       {editing ? (
-        <form onSubmit={save}>
-          <section>
-            <label htmlFor="avatar">
+        <div className="edit-profile-form-container">
+          <form className="edit-profile-form" onSubmit={save}>
+            <section className="display-flex">
+              <label htmlFor="avatar" className="profile-image-input-container">
+                <img
+                  src={
+                    auth.avatar && !accountInfo.avatar
+                      ? auth.avatar
+                      : accountInfo.avatar
+                      ? accountInfo.avatar
+                      : "../../static/default-image.png"
+                  }
+                />
+                <span style={{ cursor: "pointer" }}>
+                  Change profile picture
+                </span>
+                <input
+                  type="file"
+                  id="avatar"
+                  name="avatar"
+                  accept="image/*"
+                  ref={(x) => setFile(x)}
+                  style={{ display: "none" }}
+                ></input>
+              </label>
+            </section>
+
+            <section className="display-flex">
+              <div className="firstName-lastName-input-container">
+                <span className="display-flex">
+                  <label htmlFor="firstName">First name</label>
+                  <input
+                    type="text"
+                    id="firstName"
+                    value={accountInfo.firstName}
+                    name="firstName"
+                    onChange={onChange}
+                  ></input>
+                </span>
+                <span className="display-flex">
+                  <label htmlFor="firstName">Last name</label>
+                  <input
+                    type="text"
+                    id="lastName"
+                    value={accountInfo.lastName}
+                    name="lastName"
+                    onChange={onChange}
+                  ></input>
+                </span>
+              </div>
+
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                id="email"
+                value={accountInfo.email}
+                name="email"
+                onChange={onChange}
+              ></input>
+
+              <label htmlFor="username">Username</label>
+              <input
+                type="text"
+                id="username"
+                value={accountInfo.username}
+                name="username"
+                onChange={onChange}
+              ></input>
+
+              <label htmlFor="password">Password *</label>
+              <input
+                type="text"
+                id="password"
+                value={accountInfo.password}
+                name="password"
+                onChange={onChange}
+                placeholder="Type your old password to confirm or a new password to change"
+                required
+              ></input>
+
+              <div className="edit-profile-button-container">
+                <button className="button-small" onClick={cancel}>
+                  Cancel
+                </button>
+                <button
+                  className="button-small"
+                  onClick={save}
+                  disabled={accountInfo.password === "" ? true : false}
+                >
+                  Save
+                </button>
+              </div>
+            </section>
+          </form>
+        </div>
+      ) : (
+        <div className="profile">
+          <div className="profile-container">
+            <div className="profile-image-container">
               <img
                 src={
-                  accountInfo.avatar
-                    ? accountInfo.avatar
-                    : "../../static/default-image.png"
+                  auth.avatar ? auth.avatar : "../../static/default-image.png"
                 }
-              />
-              <span>Change profile picture</span>
-              <input
-                type="file"
-                id="avatar"
-                name="avatar"
-                accept="image/*"
-                ref={(x) => setFile(x)}
-                style={{ display: "none" }}
-              ></input>
-            </label>
-          </section>
-
-          <section>
-            <div>
-              <span>
-                <label htmlFor="firstName">First name</label>
-                <input
-                  type="text"
-                  id="firstName"
-                  value={accountInfo.firstName}
-                  name="firstName"
-                  onChange={onChange}
-                ></input>
-              </span>
-              <span>
-                <label htmlFor="firstName">Last name</label>
-                <input
-                  type="text"
-                  id="lastName"
-                  value={accountInfo.lastName}
-                  name="lastName"
-                  onChange={onChange}
-                ></input>
-              </span>
+              ></img>
             </div>
-
-            <label htmlFor="email">Email</label>
-            <input
-              type="text"
-              id="email"
-              value={accountInfo.email}
-              name="email"
-              onChange={onChange}
-            ></input>
-
-            <label htmlFor="username">Username</label>
-            <input
-              type="text"
-              id="username"
-              value={accountInfo.username}
-              name="username"
-              onChange={onChange}
-            ></input>
-
-            <label htmlFor="password">Password *</label>
-            <input
-              type="text"
-              id="password"
-              value={accountInfo.password}
-              name="password"
-              onChange={onChange}
-              placeholder="Type your old password to confirm or a new password to change"
-              required
-            ></input>
-
-            <div>
-              <button onClick={cancel}>Cancel</button>
-              <button
-                onClick={save}
-                disabled={accountInfo.password === "" ? true : false}
+            <div className="profile-content-container">
+              <h1>
+                👋 Hi, {auth.firstName} {auth.lastName}!
+              </h1>
+              <div> ✉️ {auth.email} </div>
+              <div
+                style={{ cursor: "pointer" }}
+                onClick={() => setEditing(true)}
               >
-                Save
-              </button>
+                Edit Profile
+              </div>
             </div>
-          </section>
-        </form>
-      ) : (
-        <div>
-          <div>
-            <img
-              src={auth.avatar ? auth.avatar : "../../static/default-image.png"}
-            ></img>
-          </div>
-          <div>
-            <h1>
-              👋 Hi, {auth.firstName} {auth.lastName}!
-            </h1>
-            <div> ✉️ {auth.email} </div>
-            <div onClick={() => setEditing(true)}>Edit Profile</div>
           </div>
         </div>
       )}
+
+      <Canvas>
+        <mesh>
+          <Hoodie />
+          <ambientLight />
+          <OrbitControls maxDistance={16} minDistance={9} enableZoom={false} />
+        </mesh>
+        <Environment preset="city" />
+      </Canvas>
     </div>
   );
 };
